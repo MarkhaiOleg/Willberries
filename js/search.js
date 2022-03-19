5ee74b207d78ded8a915f05dd7c5be7769f2cfda
Oleg Markhai <oleg987663@gmail.com>
2022-03-19 16:15:09 +0200

    work with JSON

--- js/search.js
+++ js/search.js
@@ -2,11 +2,10 @@ const search = function () {
     const input = document.querySelector('.search-block > input')
     const searchBtn = document.querySelector('.search-block > button')
 
-    input.addEventListener('input', (event) => {
-        searchBtn.onclick = function() {
-            console.log(event.target.value)
-        }
-    })
+    searchBtn.addEventListener('click', () => {
+        console.log(input.value)
+    }
+    )
 }
 
 search()
