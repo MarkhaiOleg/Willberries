b8fc0582a99ab1384039b63e6673f62745c82e49
Oleg Markhai <oleg987663@gmail.com>
2022-03-31 23:35:54 +0300

    4 lesson glo academy

--- js/cart.js
+++ js/cart.js
@@ -1,15 +1,36 @@
 const cart = function () {
-const cartBtn = document.querySelector('.button-cart')
-const cart = document.getElementById('modal-cart')
-const closeBtn = cart.querySelector('.modal-close')
+    const cartBtn = document.querySelector('.button-cart')
+    const cart = document.getElementById('modal-cart')
+    const closeBtn = cart.querySelector('.modal-close')
+    const goodsContainer = document.querySelector('.long-goods-list')
 
-cartBtn.addEventListener('click', () => {
-    cart.style.display = 'flex'
-})
+    const addToCart = (id) => {
+        const goods = JSON.parse(localStorage.getItem('goods'))
+        const clickedGood = goods.find(good => good.id === id)
+        const cart = localStorage.getItem('cart') ?
+            JSON.parse(localStorage.getItem('cart')) : []
+        console.log(clickedGood)
+        console.log(cart)
+    }
 
-closeBtn.addEventListener('click', () => {
-    cart.style.display = ''
-})
+
+    cartBtn.addEventListener('click', () => {
+        cart.style.display = 'flex'
+    })
+
+    closeBtn.addEventListener('click', () => {
+        cart.style.display = ''
+    })
+    if (goodsContainer) {
+        goodsContainer.addEventListener('click', (event) => {
+            if (event.target.closest('.add-to-cart')) {
+                const buttonToCart = event.target.closest('.add-to-cart')
+                const goodId = buttonToCart.dataset.id
+
+                addToCart(goodId)
+            }
+        })
+    }
 }
 
 cart()
